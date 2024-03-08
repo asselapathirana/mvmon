0928784b0c1d70e54789a25de7b7a4bbcb946af9
Assela Pathirana <apa@un-ihe.org>
2024-03-08 13:12:14 +0100

    reading password from file

--- loginornot.py
+++ loginornot.py
@@ -1,9 +1,12 @@
 #dash-auth==2.2.1
 import dash_auth
 
-VALID_USERNAME_PASSWORD_PAIRS = {
-    'hello': 'world'
-}
+version = "secure version"
+
+# read ./data/.password file as json
+import json
+with open('./data/.password') as f:
+    VALID_USERNAME_PASSWORD_PAIRS = json.load(f)
 
 def get_auth(app):  
     auth = dash_auth.BasicAuth(
