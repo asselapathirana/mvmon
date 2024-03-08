e7638542116218f3657462ff8220395d4719bfa3
Assela Pathirana <apa@un-ihe.org>
2024-03-08 11:05:48 +0100

    public sends auth=None

--- loginornot.py
+++ loginornot.py
@@ -1,9 +1,17 @@
-from app import app as app
+#dash-auth==2.2.1
+import dash_auth
+
 VALID_USERNAME_PASSWORD_PAIRS = {
     'hello': 'world'
 }
 
-auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
-)
+""" def get_auth(app):  
+    auth = dash_auth.BasicAuth(
+        app,
+        VALID_USERNAME_PASSWORD_PAIRS
+    )
+    return auth
+    """
+    
+def get_auth(app):
+        return None
